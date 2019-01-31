4255e93c9078c7f3253491dcf2d12b21b4fed46c
Adam Gordon <adamjgordon24@gmail.com>
2019-01-31 04:44:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,7 +41,6 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   grouped_supplies = holiday_hash[:winter].values.flatten
-  grouped_supplies.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
