4fe8f8e63d802451a8b799976b37dd5c61e384bd
Adam Gordon <adamjgordon24@gmail.com>
2019-01-31 04:41:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,7 +35,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[:fall].values = [{:thanksgiving => ["Turkey"], :holiday_name => supply_array}]
+  holiday_hash[:fall][holiday_name] = supply_array
 holiday_hash
 end
 
