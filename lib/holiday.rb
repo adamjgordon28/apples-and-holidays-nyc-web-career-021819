94bf81e85cc5d63443cf7425923e7d8c9a532ce3
Adam Gordon <adamjgordon24@gmail.com>
2019-01-31 05:26:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
     season_word = season.to_s.capitalize!
     puts "#{season_word}:"
     holiday.each do |holiday,supplies|
-      supplies_list = supplies.join(" ")
+      supplies_list = supplies.join(", ")
       puts "#{holiday}: #{supplies_list}"
     end
   end
