c93d0729ff3dcfc679a3f478816d451259e9be2f
Adam Gordon <adamjgordon24@gmail.com>
2019-01-31 04:25:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,7 +29,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season,holiday|
     if season == :winter
       holiday.each do |supplies|
-        supplies << "Balloons"
+        supplies << supply
       end
     end 
   end
