5c25ec91a0d3a21137683e60b551957bed4bf9bd
Adam Gordon <adamjgordon24@gmail.com>
2019-01-31 04:30:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   
   holiday_hash[:winter].collect do |holiday,supplies|
-    holidays.each do |supplies|
+    holiday.each do |supplies|
       supplies << supply
     end
   end
