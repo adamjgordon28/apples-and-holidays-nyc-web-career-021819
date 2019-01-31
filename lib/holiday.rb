4baaa5d7a0ee91592511fffce2e7bc216c87dac2
Adam Gordon <adamjgordon24@gmail.com>
2019-01-31 04:47:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,8 +53,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
   holiday_hash.each do |season, holiday|
-    season.each do |holiday, supplies|
-      puts 
+    puts #{season}
   end
 
 end
