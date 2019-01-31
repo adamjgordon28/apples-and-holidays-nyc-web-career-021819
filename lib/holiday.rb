182c7ae5f2b6e91a7ba5f74eaca921c751f69e82
Adam Gordon <adamjgordon24@gmail.com>
2019-01-31 05:13:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,8 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season,holiday|
     holiday.collect do |holiday,supplies|
       if supplies.include?("BBQ")
-       return holiday
+        holiday
+      end
     end
   end
 end
