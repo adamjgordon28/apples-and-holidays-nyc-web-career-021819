3bbba00e35bdf63f419213393257ff7e65d7a19f
Adam Gordon <adamjgordon24@gmail.com>
2019-01-31 05:14:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,8 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+  bbq_array = []
   holiday_hash.collect do |season,holiday|
     holiday.select do |holiday,supplies|
       if supplies.include?("BBQ")
