b87d0d378e70de98f08ee4e90701884328f8bc7f
Adam Gordon <adamjgordon24@gmail.com>
2019-01-31 04:45:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,6 +52,11 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each do |season, holiday|
+    season.each do |holiday, supplies|
+      puts 
+  end
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
